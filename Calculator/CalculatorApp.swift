5f346314ed02d9a00ab42e90206c962fde1b614a
Keila Islas <kislas@bsd220.org>
2023-12-07 09:46:31 -0600

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Keila Islas on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
